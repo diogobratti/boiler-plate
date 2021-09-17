1d398f44591d5a9af9136910fd96f58ab07a589c
Diogo Bratti <dbratti@gmail.com>
2021-09-17 18:00:23 -0300

    https server

--- server.js
+++ server.js
@@ -1,11 +1,38 @@
-require('dotenv').config();
-const app = require('./app');
-require('./redis');
+require("dotenv").config();
+const app = require("./app");
+require("./redis");
+const https = require("https");
+const fs = require("fs");
 
-
-const port = (process.env.PORT ? process.env.PORT : 3333);
-
-const routes = require('./routes');
+const routes = require("./routes");
 routes(app);
 
-app.listen(port, () => console.log(`App listening on port ${port}`));
+const port = process.env.PORT ? process.env.PORT : 3333;
+
+if (process.env.NODE_ENV == "production") {
+  // Certificate
+  const privateKey = fs.readFileSync(
+    `${process.env.CERTBOT_FOLDER}/privkey.pem`,
+    "utf8"
+  );
+  const certificate = fs.readFileSync(
+    `${process.env.CERTBOT_FOLDER}/cert.pem`,
+    "utf8"
+  );
+  const ca = fs.readFileSync(`${process.env.CERTBOT_FOLDER}/chain.pem`, "utf8");
+
+  const credentials = {
+    key: privateKey,
+    cert: certificate,
+    ca: ca,
+  };
+
+  // Starting both http & https servers
+  const httpServer = http.createServer(app);
+  const httpsServer = https.createServer(credentials, app);
+  httpsServer.listen(port, () => {
+    console.log(`HTTPS Server running on port ${port}`);
+  });
+} else {
+  app.listen(port, () => console.log(`App listening on port ${port}`));
+}
